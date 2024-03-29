b7427ab7cfe9a8224a4b729f603b2fb1888f82c8
OneCODE2 <109349689+OneCODE2@users.noreply.github.com>
2024-03-30 00:53:51 +0530

    Add files via upload

---
+++ To_Do_List/1.js
@@ -0,0 +1,80 @@
+window.addEventListener('load', () => {
+    document.getElementById('list').style.height = document.documentElement.clientHeight - document.querySelector('.controls').offsetHeight - 28 + 'px'
+});
+
+
+    inputTask.addEventListener('focus', function(){
+
+        label.style.top = '0'
+        label.style.color =  `#3C87FF`
+
+    })
+    inputTask.addEventListener('blur', function(){
+
+        if(this.value === ''){
+            label.style.top = '50%'
+            label.style.color =  `#bfbfbf`
+        }else{
+            label.style.top = '0'
+            label.style.color =  `#bfbfbf`
+        }
+
+    })
+
+    buttons.forEach(btn => {
+
+        btn.addEventListener('mousedown', function(){
+            this.style.transform = 'scale(0.95)'
+        })
+
+        btn.addEventListener('mouseup', function(){
+            this.style.transform = 'scale(1)'
+        })
+
+    })
+
+    tabName.forEach(item => {
+
+        item.addEventListener('click', function(){
+
+            document.querySelector('.tabName.active')?.classList.remove('active')
+            item.classList.add('active')
+
+            document.querySelector('.tabList .line').style.left = item.offsetLeft + 'px'
+            document.querySelector('.tabList .line').style.width = item.offsetWidth + 'px'
+            
+            document.querySelectorAll('.tabContent ul').forEach(ul => {
+                if(ul.dataset.pane === item.id){
+                    document.querySelector('.tabContent ul.active')?.classList.remove('active')
+                    ul.classList.add('active')
+                }
+            })
+
+        })
+        
+    })
+    document.querySelector('.tabName.active')?.click()
+//-----------------------------------------------------
+
+//4. hiệu ứng tabs của menu show
+    document.querySelectorAll('.tabPaneShow li').forEach(item => {
+
+        item.addEventListener('click', function(){
+            document.querySelector('.tabPaneShow li.active')?.classList.remove('active')
+            item.classList.add('active')
+        })
+
+    })
+//-----------------------------------------------------
+
+//5. hiệu ứng ẩn hiện menu detele và edit
+    function dotClick(_this){ 
+        document.querySelector('.dot.active')?.classList.remove('active')
+        _this.classList.add('active')
+    }
+    window.addEventListener('click', function(e){
+        if(!e.target.matches('.dot.active')){
+            document.querySelector('.dot.active')?.classList.remove('active')
+        }
+    })
+//-----------------------------------------------------
